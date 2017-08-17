d83d55c8089959326f1a247ce0fdc7660a08cccd
Thomas Sauvajon <thomas.sauvajon@gmail.com>
2017-08-17 19:18:11 +0200

    Added day 3

--- 30-days-challenge/2-operators/main.go
+++ 30-days-challenge/2-operators/main.go
@@ -1,10 +1,16 @@
 package main
-func main() {
-	var (
-		meal, tip, tax float64
-	)
 
-	fmt.Scan(&meal)
-	fmt.Scan(&tip)
-	fmt.Scan(&tax)
+import "fmt"
+
+func main() {
+    var (
+        meal, tip, tax float64
+    )
+    
+    fmt.Scan(&meal)
+    fmt.Scan(&tip)
+    fmt.Scan(&tax)
+    
+    total := meal * (1 + (tip / 100) + (tax / 100))
+    fmt.Printf("The total meal cost is %d dollars.", int(total + .5))
 }
---
+++ 30-days-challenge/3-intro-to-conditionnal-statements/main.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main() {
+    var n int
+    
+    fmt.Scan(&n)
+    
+    switch {
+        case n % 2 == 1 || (n >= 6 && n <= 20):
+            fmt.Print("Weird")
+        default:
+            fmt.Print("Not Weird")
+    }
+}
