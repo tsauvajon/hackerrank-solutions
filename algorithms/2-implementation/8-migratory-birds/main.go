c30a546f454372e7b3909db3d631970f0787216e
Thomas Sauvajon <thomas.sauvajon@gmail.com>
2017-08-21 14:14:29 +0200

    Added Migratory Birds Challenge

---
+++ algorithms/2-implementation/8-migratory-birds/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		n, max int
+		birds  []int
+		count  [5]int
+	)
+
+	fmt.Scan(&n)
+
+	birds, _ = intScanln(n)
+
+	for _, birdType := range birds {
+		count[birdType-1]++
+	}
+
+	for i := range count {
+		if count[i] > count[max] {
+			max = i
+		}
+	}
+
+	fmt.Print(max)
+}
+
+func intScanln(n int) ([]int, error) {
+	x := make([]int, n)
+	y := make([]interface{}, len(x))
+	for i := range x {
+		y[i] = &x[i]
+	}
+	n, err := fmt.Scanln(y...)
+	x = x[:n]
+	return x, err
+}
