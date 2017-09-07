f53a67b6d7a98df40e560c3833c7af004e16ae21
Thomas Sauvajon <thomas.sauvajon@gmail.com>
2017-09-07 16:51:52 +0200

    Added Day 27

---
+++ 30-days-challenge/27-testing/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("5")
+
+	fmt.Println("5 3")
+	fmt.Println("-1 90 999 100 0")
+
+	fmt.Println("4 2")
+	fmt.Println("0 -1 2 1")
+
+	fmt.Println("3 3")
+	fmt.Println("-1 0 1")
+
+	fmt.Println("6 1")
+	fmt.Println("-1 0 1 -1 2 3")
+
+	fmt.Println("7 3")
+	fmt.Println("-1 0 1 2 3 4 5")
+}
